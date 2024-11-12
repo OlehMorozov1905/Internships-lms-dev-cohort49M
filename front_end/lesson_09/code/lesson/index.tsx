3ae8fb744f5f7127f1d3e2ac33e372bfa98cf447
mohnapne <kuvaldin2012@gmail.com>
2024-11-12 14:00:19 +0100

    lesson 9

---
+++ front_end/lesson_09/code/lesson/index.tsx
@@ -0,0 +1,24 @@
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import Lesson09 from "./lessons/lesson09/Lesson09";
+
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+root.render(
+  <>
+    {/* <Lesson06/> */}
+    {/* <Lesson04 /> */}
+    {/* <Lesson07 /> */}
+    {/* <Lesson05 /> */}
+    {/* <Lesson08/> */}
+    {/* <FetchDog/> */}
+    {/* <FetchFox/> */}
+    <Lesson09 />
+  </>
+);
+
+
+
+
---
+++ front_end/lesson_09/theory.md
@@ -0,0 +1,4 @@
+### React: Controlled and uncontrolled components, useEffect
+
+
+Скачать презентацию можно в разделе code
